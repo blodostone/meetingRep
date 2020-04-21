6e111d510c300a068fb982e1014cee35bf3cde44
bloodstone <maks.khokholov.99@gmail.com>
2020-04-21 12:05:19 +0300

    lklk

--- metting21.04.2020/metting2/index.js
+++ metting21.04.2020/metting2/index.js
@@ -2,19 +2,13 @@
 // output: bool (true/false)
 
 function some(arr) {
-    for (let g = 0; g < arr.length; g += 1) {
-        console.log('==> run');
-
-        if (arr[g] % 3 !== 0) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 3 === 0) {
             return true;
         }
-
     }
     return false;
 }
-
-const number = [32, 56, 3, 4, 55];
-
-const result = some(number);
-
-console.log(result);
+const numbers = [3, 5, 6, 6, 7]
+const result = some(numbers);
+console.log(result)
