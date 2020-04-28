c674121aec0c10515aab38baa320d4eb667b64f5
bloodstone <maks.khokholov.99@gmail.com>
2020-04-28 13:07:00 +0300

    hnf

--- metting28.04.2020/index.js
+++ metting28.04.2020/index.js
@@ -1,16 +1,16 @@
 const numbersList = [4.543, 6.7, 3.35, 2.89];
 
-function callback(acc, num) {
-    console.log(acc, num);
-    const newAcc = acc + num;
-    // что то счетаем...
-    // return newAcc;
-    return undefined;
-}
+// function callback(acc, num) {
+//     console.log(acc, num);
+//     const newAcc = acc + num;
+//     // что то счетаем...
+//     // return newAcc;
+//     return undefined;
+// }
 
-const getTotalPrice = numbersList.reduce(callback, 0);
+// const getTotalPrice = numbersList.reduce(callback, 0);
 
-console.log('result: ', sum);
+// console.log('result: ', sum);
 
 
 
