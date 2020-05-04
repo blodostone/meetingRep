a3c7fc3d7a83e3446356001e07260cafc789f147
bloodstone <maks.khokholov.99@gmail.com>
2020-05-04 12:59:45 +0300

    .m,.

---
+++ metting04.05.2020/task1/index.js
@@ -0,0 +1,25 @@
+// input: userData, id
+// output: object;
+
+const userData = {
+    name: 'Sam'
+}
+
+function addPropertyV1(userData, userId) {
+    userData.id = userId;
+    return userData;
+}
+
+function addPropertyV2(userData, userId) {
+    Object.assign(userData, { id: userId });
+    return userData;
+}
+
+function addPropertyV3(userData, userId) {
+    Object.assign({}, userData, { id: userId });
+    return userData;
+}
+
+function addPropertyV4(userData, userId) {
+    return {...userData, id: userId }
+}
