f2519a294a1cde72b8d2a5ccd21e3381c4de53c7
bloodstone <maks.khokholov.99@gmail.com>
2020-05-05 13:29:40 +0300

    ytr

---
+++ metting05.05.2020/task1/index.js
@@ -0,0 +1,32 @@
+// input: array, array;
+//output: object;
+
+
+// function buildObject (keysList, valuesList) {
+
+//     let obj = {};
+
+//     for(let i = 0; i < keysList.length; i++){
+
+//         obj = {...obj, [keysList[i]]: valuesList[i] }
+
+//     }
+
+//     return obj;
+
+// }
+
+function buildObjectV2(keysList, valuesList) {
+    let obj = keysList.raduce((acc, key, index) => {
+        return {...acc, [key]: valuesList[index] };
+    }, obj);
+}
+
+// const keys = ['name', 'age'];
+// const values = ['Tom', 40];
+
+// function buildObjectV3 (keysList, valuesList) {
+//     let obj = keysList.raduce((acc, key, index) => {
+//         return {[key]: valuesList[index] };
+//     }, obj);
+// }
