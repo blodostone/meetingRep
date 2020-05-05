b8e829655ce0b725ccfe193bda209142bf57260b
bloodstone <maks.khokholov.99@gmail.com>
2020-05-05 13:36:08 +0300

    gh

--- metting05.05.2020/task1/index.js
+++ metting05.05.2020/task1/index.js
@@ -2,25 +2,30 @@
 //output: object;
 
 
-// function buildObject (keysList, valuesList) {
-
-//     let obj = {};
-
-//     for(let i = 0; i < keysList.length; i++){
-
-//         obj = {...obj, [keysList[i]]: valuesList[i] }
-
-//     }
-
-//     return obj;
-
-// }
+const keys = ['name', 'age'];
+const values = ['Tom', 40];
 
 function buildObject(keysList, valuesList) {
-    let obj = keysList.reduce((acc, key, index) => {
-        return {...acc, [key]: valuesList[index] };
-    }, obj);
+
+    let obj = {};
+
+    for (let i = 0; i < keysList.length; i++) {
+
+        obj = {...obj, [keysList[i]]: valuesList[i] }
+
+    }
+
+    return obj;
+
 }
+
+// function buildObject(keysList, valuesList) {
+//     let obj = keysList.reduce((acc, key, index) => {
+//         return {...acc, [key]: valuesList[index] };
+//     }, obj);
+// }
+
+console.log(buildObject(keys, values));
 
 // const keys = ['name', 'age'];
 // const values = ['Tom', 40];
