7e83e03930b1a6c3a0619805f115cc96b63fbfe5
bloodstone <maks.khokholov.99@gmail.com>
2020-05-06 10:33:48 +0300

    hgk

---
+++ metting06.05.2020/task1/index.js
@@ -0,0 +1,29 @@
+//input obj1, obj2
+
+
+//algo
+// compare keys count
+//comape values
+
+const obj1 = {
+    name: 'Tom',
+    age: 17
+};
+const obj2 = {
+    name: 'Tom',
+    age: 17
+};
+
+
+
+function compareObjects(obj1, obj2) {
+
+    const keys = Object.keys(obj1);
+    const keys = Object.keys(obj2);
+
+    if (keys1.length !== keys2.length) {
+        return false;
+    }
+
+    return keys1.reduce((acc, key) => obj1[key] === obj2[key], true);
+}
