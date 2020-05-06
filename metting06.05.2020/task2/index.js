9f80e60dd3c19d5ba84f520ecf3f6d84ad885cd0
bloodstone <maks.khokholov.99@gmail.com>
2020-05-06 10:48:11 +0300

    hg

--- metting06.05.2020/task2/index.js
+++ metting06.05.2020/task2/index.js
@@ -4,6 +4,13 @@
 //algo:
 //iterate array - if user is admin true, if not - mark false
 
+const users = [
+    { id: '1', name: 'Bob' },
+    { id: '2', name: 'Tom' },
+    { id: '3', name: 'Sam' },
+    { id: '4', name: 'Tad' },
+];
+
 function markAdmins(users, ids) {
     return users.map(user => {
         const isAdmin = ids.includes(id);
