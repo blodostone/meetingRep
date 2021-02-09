1fb6aaf66f358f7194e13186d6d8aff8f0891934
bloodstone <maks.khokholov.99@gmail.com>
2021-02-09 11:33:25 +0200

    gfdg

--- react/13less/src/App.jsx
+++ react/13less/src/App.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Link, Switch } from 'react-router-dom';
 import Users from './Users';
 import Home from './Home';
 
-
-function App () {
-    return (
-        <BrowserRouter>
-            <div className="page">
-                <ul className="navigation">
-                    <li className="navigation__item"><Link to="/">Home</Link></li>
-                    <li className="navigation__item"><Link to="/users">Users</Link></li>
-                </ul>
-                <Switch>
-                    <Route exact path="/"><Home/></Route>
-                    <Route exact path="/users" component={Users}></Route>
-                </Switch>
-            </div>
-        </BrowserRouter>
-    );
+const App = () => {
+  return (
+    <Router>
+      <div className="page">
+        <ul className="navigation">
+          <li className="navigation__item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="navigation__item">
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
--- react/13less/src/User.jsx
+++ react/13less/src/User.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const User = () => {
+  const [userData, setUserData] = useState(null);
+  const { userId } = useParams();
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${userId}`)
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+
+        throw new Error();
+      })
+      .then(userData => {
+        setUserData(userData);
+      });
+  }, [userId]);
+
+  if (!userData) {
+    return null;
+  }
+
+  const { name, location, avatar_url } = userData;
   return (
     <div className="user">
-      <img
-        alt="User Avatar"
-        src="https://avatars1.githubusercontent.com/u/9919?v=4"
-        className="user__avatar"
-      />
+      <img alt="User Avatar" src={avatar_url} className="user__avatar" />
       <div className="user__info">
-        <span className="user__name">GitHub</span>
-        <span className="user__location">San Francisco,CA</span>
+        <span className="user__name">{name}</span>
+        <span className="user__location">{location}</span>
       </div>
     </div>
   );
